c39057b674f93ed8268778c1e209803ee0d1be4b
A1lisa <aleksandra071078@icloud.com>
2026-03-17 13:53:28 +0800

    Добавьте файлы проекта.

---
+++ gui3/Temperature.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Температура, задаваемая в виде пары (значение, тип), допустимые типы:
+градус Цельсия, градус Фаренгейта, градус Ранкина, Кельвины
+сложение
+вычитание
+умножение на число
+сравнение
+вывод значения в любом типе
+4
+Скорость заданная в виде пары (значение*/
+namespace gui3
+{
+    public enum MeasureType { C, F, Ra, K };
+
+    public class Temperature
+    {
+        private double value;
+        private MeasureType type;
+
+        public Temperature(double value, MeasureType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+        public string Verbose()
+        {
+            string typeVerbose = "";
+            switch (this.type)
+            {
+                case MeasureType.C:
+                    typeVerbose = "°C";
+                    break;
+                case MeasureType.F:
+                    typeVerbose = "°F";
+                    break;
+                case MeasureType.Ra:
+                    typeVerbose = "°Ra";
+                    break;
+                case MeasureType.K:
+                    typeVerbose = "K";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeVerbose);
+        }
+
+        public static Temperature operator+(Temperature temp, double number)
+        {
+            var newValue = temp.value + number;
+            var temperature = new Temperature(newValue, temp.type);
+            return temperature;
+        }
+
+        public static Temperature operator +(double number, Temperature temp) {
+            return temp + number;
+
+    }
+
+    
+}
---
+++ gui3Tests/TemperatureTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using gui3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace gui3.Tests
+{
+    [TestClass()]
+    public class TemperatureTests
+    {
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            var temp = new Temperature(15, MeasureType.C);
+            Assert.AreEqual("15 °C", temp.Verbose());
+
+            temp= new Temperature(15, MeasureType.F);
+            Assert.AreEqual("15 °F", temp.Verbose());
+
+            temp = new Temperature(15, MeasureType.Ra);
+            Assert.AreEqual("15 °Ra", temp.Verbose());
+
+            temp = new Temperature(15, MeasureType.K);
+            Assert.AreEqual("15 K", temp.Verbose());
+
+        }
+        [TestMethod()]
+        public void AddNumberTest()
+        {
+            var temp=new Temperature(1, MeasureType.C);
+            temp = temp + 10.5;
+            Assert.AreEqual("11.5 °C", temp.Verbose());
+        }
+
+
+    }
+}
